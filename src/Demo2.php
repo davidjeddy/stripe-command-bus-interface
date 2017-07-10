2d7297fb9cacff03d43bd538eb581892d2fa615c
David J Eddy <me@davidjeddy.com>
2017-07-09 20:29:13 -0400

    MOVED demo's into example dir

--- src/Demo2.php
+++ src/Demo2.php
@@ -8,8 +8,12 @@ use League\Tactician\Handler\MethodNameInflector\HandleClassNameInflector;
 // Our example Command and Handler. ///////////////////////////////////////////
 class RegisterUserCommand
 {
-    public $emailAddress;
-    public $password;
+    public function __construct($data)
+    {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        }
+    }
 }
 
 class RegisterUserHandler
@@ -17,12 +21,14 @@ class RegisterUserHandler
     public function handleRegisterUserCommand(RegisterUserCommand $command)
     {
         // Do your core application logic here. Don't actually echo stuff. :)
-        echo "User {$command->emailAddress} was registered!\n";
+        echo "User {$command->emailAddress} was registered using `{$command->password}` as a password.\n";
     }
 }
 
 // Setup the bus, normally in your DI container ///////////////////////////////
 $locator = new InMemoryLocator();
+// Register handler
+// *->addHandler($handler, $commandClassName)
 $locator->addHandler(new RegisterUserHandler(), RegisterUserCommand::class);
 
 // Middleware is Tactician's plugin system. Even finding the handler and executing it is a plugin that we're configuring here.
@@ -35,8 +41,8 @@ $handlerMiddleware = new League\Tactician\Handler\CommandHandlerMiddleware(
 $commandBus = new \League\Tactician\CommandBus([$handlerMiddleware]);
 
 // Controller Code ////////////////////////////////////////////////////////////
-$command = new RegisterUserCommand();
-$command->emailAddress = 'alice@example.com';
-$command->password = 'secret';
-
+$command = new RegisterUserCommand([
+    'emailAddress'  => 'alice@example.com',
+    'password'      => 'secret'
+]);
 $commandBus->handle($command);

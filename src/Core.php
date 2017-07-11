fc7b99cc163c30e308589a5ac68f17cdd0d17a85
David J Eddy <me@davidjeddy.com>
2017-07-11 12:03:03 -0400

    ADDED init handler classes ADDED\UPDATED composer.json

---
+++ src/Core.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace common\commands\Stripe;
+
+use yii\base\Object;
+use trntv\bus\interfaces\SelfHandlingCommand;
+
+/**
+ * @author David J Eddy <me@davidjeddy.com>
+ */
+class Core extends Object implements SelfHandlingCommand
+{
+    /**
+     * @var string
+     */
+    public $noun = '';
+
+    /**
+     * @var string
+     */
+    public $verb = 'create';
+
+    /**
+     * @var mixed
+     */
+    public $data = null;
+
+    /**
+     * @var string
+     */
+    protected $privateKey;
+
+    /**
+     * Command init
+     * Set the keys to obj->prop
+     */
+    public function init()
+    {
+        \Stripe\Stripe::setApiKey(env('STRIPE_PRV_KEY'));
+    }
+
+    /**
+     * @param $command
+     * @return string
+     * @throws \Exception
+     */
+    public function handle($command)
+    {
+        if (!$this->noun ||!$this->verb) {
+            throw new \Exception('Please provide noun target and verb action in command bus parameters array.');
+        }
+
+        try {
+            $class = '\common\commands\Stripe\\' . ucfirst($this->noun). '\\' . ucfirst($this->verb);
+            $class = new $class;
+
+            return $class->handle($command);
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+}
